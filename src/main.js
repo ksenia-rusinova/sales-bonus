6c421f4573f4c4956714e792f53105b993e6ad0e
Ksenia Rusinova <rusinovakseniaolegovna@yandex.ru>
2025-12-28 14:34:52 +0400

    fix

--- src/main.js
+++ src/main.js
@@ -24,13 +24,13 @@ function calculateBonusByProfit(index, total, seller) {
   const { profit } = seller;
 
   if (index === 0) {
-    return 0.15;
+    return seller.profit * 0.15;
   } else if (index === 1 || index === 2) {
-    return 0.1;
+    return seller.profit * 0.1;
   } else if (index === total - 1) {
-    return 0;
+    return seller.profit * 0;
   } else {
-    return 0.05;
+    return seller.profit * 0.05;
   }
 }
 
@@ -143,8 +143,7 @@ function analyzeSalesData(data, options) {
   // @TODO: Назначение премий на основе ранжирования
   sellerStats.forEach((seller, index) => {
     // Считаем бонус
-    seller.bonus =
-      seller.profit * calculateBonus(index, sellerStats.length, seller);
+    seller.bonus = calculateBonus(index, sellerStats.length, seller);
 
     // Формируем топ-10 товаров
     const entries = Object.entries(seller.products_sold);
